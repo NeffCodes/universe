b9bbac292cb2b8b5001e923a36c621325007a78d
James Neff <james.m.neff@gmail.com>
2022-04-29 15:22:23 -0400

    Add other pulled images

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,16 @@
 fetch('/.netlify/functions/getPhotos')
 	.then(res => res.json()) // parse response as JSON
 	.then(data => {
+		data = data.reverse()
 		console.log(data)
 		console.log(data.url)
 
-		document.querySelector('img').src = data.url
-		document.querySelector('h2').innerText = data.title;
-		document.querySelector('p').innerText = data.explanation
+		// document.querySelector('img').src = data.url
+		// document.querySelector('h2').innerText = data.title;
+		// document.querySelector('p').innerText = data.explanation
+		let main = document.querySelector('main');
+		let imgs = main.querySelectorAll('a')
+		imgs.forEach( (a,i) => a.style.backgroundImage = `url(${data[i].url})` )
 	})
 	.catch(err => {
           console.log(`error ${err}`)
