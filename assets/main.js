56e39fb44b03a4fec5f57131d03c7cbff4629d99
James Neff <james.m.neff@gmail.com>
2022-05-05 17:15:34 -0400

    create function to build out image cards per image pulled

--- assets/main.js
+++ assets/main.js
@@ -8,10 +8,42 @@ fetch('/.netlify/functions/getPhotos')
 		// document.querySelector('img').src = data.url
 		// document.querySelector('h2').innerText = data.title;
 		// document.querySelector('p').innerText = data.explanation
-		let main = document.querySelector('main');
-		let imgs = main.querySelectorAll('a')
-		imgs.forEach( (a,i) => a.style.backgroundImage = `url(${data[i].url})` )
+		let gallery = document.querySelector('.gallery');
+
+		data.forEach( img => createImageCard(img))
+		// let imgs = main.querySelectorAll('a')
+		// imgs.forEach( (a,i) => {
+		// 	console.log('aasdf',a,i)
+		// 	createCard(a,data[i].url)
+		// } )
 	})
 	.catch(err => {
           console.log(`error ${err}`)
   })
+
+	function createImageCard( imgData ){
+		const gallery = document.querySelector('.gallery');
+		//create each node
+		const article = document.createElement('article');
+		const anchor = document.createElement('a');
+		const img = document.createElement('img');
+		const h2 = document.createElement('h2');
+		const p = document.createElement('p');
+
+		//add data to nodes
+		h2.innerText = imgData.title;
+		img.src = imgData.url;
+		// p.innerText = imgData.explanation;
+		
+		//style nodes
+		article.classList.add('tile')
+
+		//build out card tree
+		anchor.appendChild(img);
+		article.appendChild(anchor);
+		article.appendChild(h2);
+		article.appendChild(p);
+
+		gallery.appendChild(article);
+	}
+
